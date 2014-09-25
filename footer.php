d63661d962554c7fbb0ace257045865cd9422ccd
Nicholas Avenell <nicholas@istic.net>
2014-09-25 11:19:06 +0200

    Initial customisation

---
+++ footer.php
@@ -0,0 +1,11 @@
+        <div id="footer">
+        	<div id="copyright">&copy; 2010 - <?php echo date("Y") ?> Istic.Networks ltd<br>
+	</div>
+	<div id="credits">
+		UK Registered company <a href="http://opencorporates.com/companies/gb/07439954">07439954</a>
+		[<a href="/contact">Contact</a>]
+        </div>
+    </div>
+<?php wp_footer(); ?>
+</body>
+</html>
